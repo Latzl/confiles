3bbb190668e6c5aa63a64990def3f1421940a928
latzl <theaccount_latzl@163.com>
2025-02-18 01:40:48 +0800

    \t to indent

--- mods/utils/home/bin/jsonc2json.py
+++ mods/utils/home/bin/jsonc2json.py
@@ -16,6 +16,6 @@ with open(input_path, 'r') as f:
     data = json5.load(f)
 
 with open(output_path, 'w') as f:
-    json.dump(data, f, indent=2)
+    json.dump(data, f, indent='\t')
 
 print(f"Converted: {input_path} -> {output_path}")
